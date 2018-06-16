8efc7c29d1a5f8302f97d228f214dcd031c0f420
mattlokes <matlokes@gmail.com>
2018-06-16 13:54:31 +0000

    Changes to photodb app

--- photodb/main.py
+++ photodb/main.py
@@ -11,30 +11,29 @@ api = Api(app)
 
 
 class PhotoDB(Resource):
-    def get(self, collection_name):
+    def get(self, collection_name, primary):
         conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from {0} where photo_primary = 1".format(collection_name) )
+        query = conn.execute("select * from {0} where photo_primary = {1}".format(collection_name, primary) )
         #print query.cursor.fetchall()
         return {'photos': [ (i[1],i[2],i[3]) for i in query.cursor.fetchall()]}
     
-    def post(self,collection_name):
+    def post(self,collection_name, primary):
         conn = db_connect.connect()
         print(request.json)
         #photo_id = ????
         photo_name = request.json['photo_name']
         photo_link = request.json['photo_link']
         photo_tiny_link = tinyurl.create_one(str(photo_link))
-        photo_primary = request.json['photo_primary']
-        photo_primary_conv = int(photo_primary)
+        photo_primary = int(primary)
         
         query = conn.execute("insert into {0} values(null,'{1}','{2}','{3}',{4})".format(
                              collection_name,photo_name, 
-                             photo_link, photo_tiny_link, photo_primary_conv))
+                             photo_link, photo_tiny_link, photo_primary))
         return {'status':'success', 'slink': photo_tiny_link}
 
 
 
-api.add_resource(PhotoDB, '/photodb/<collection_name>')
+api.add_resource(PhotoDB, '/<collection_name>/<primary>')
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=17120 )
+     app.run( port=17120 )
